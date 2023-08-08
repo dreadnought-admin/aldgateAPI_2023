f6e986fec5ed41d5ec3da75df8d2c0f362056f5b
dreadnought-admin <dom.alexan@gmail.com>
2023-08-08 16:32:32 -0600

    Creates models for menu

--- api/api/settings.py
+++ api/api/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'live_api',
+    'live_api.apps.LiveApiConfig'
     'corsheaders'
 ]
 
--- api/live_api/models.py
+++ api/live_api/models.py
@@ -1,3 +1,115 @@
 from django.db import models
+# from django.contrib.auth.models import User
 
 # Create your models here.
+
+SIZE_CHOICES = (
+    ('M', 'Medium'),
+    ('S', 'Small'),
+    ('L', 'Large'),
+    ('XL', 'Xtra-large')
+)
+
+class Burger(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=150, null=True)
+    price = models.FloatField(default=0, null=True)
+    gluten_free = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Fish(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=150, null=True)
+    price = models.FloatField(default=0, null=True)
+    size = models.CharField(max_length=10, choices=SIZE_CHOICES, default=None, null=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Chicken(models.Model):
+    name = models.Charfield(max_length=30)
+    price = models.FloatField(default=0, null=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Fries(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=150, null=True)
+    price = models.FloatField(default=0, null=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Snack(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.FloatField(default=0, null=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Wing(models.Model):
+    kilos = name = models.CharField(max_length=30)
+    price = models.FloatField(default=0, null=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Wrap(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=150, null=True)
+    price = models.FloatField(default=0, null=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Hotdog(models.Model):
+    name = models.CharField(max_length=25)
+    description = models.CharField(max_length=150, null=True)
+    price = models.FloatField(default=0, null=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Side(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.FloatField(default=0, null=False)
+    size = models.CharField(max_length=10, choices=SIZE_CHOICES, default=None, null=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Special(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=250, null=True)
+    price = models.FloatField(default=0, null=False)
+    size = models.CharField(max_length=10, choices=SIZE_CHOICES, default=None, null=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class TradPack(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=150, null=True)
+    price = models.FloatField(default=0, null=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class SpecialPack(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=150, null=True)
+    price = models.FloatField(default=0, null=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Extra(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.FloatField(default=0, null=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
