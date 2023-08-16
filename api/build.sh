9c436dd376500ee561f63a155f9766543c61e7e6
dreadnought-admin <dom.alexan@gmail.com>
2023-08-16 10:26:31 -0600

    Enables pip3 freeze command

--- api/build.sh
+++ api/build.sh
@@ -1,6 +1,7 @@
 set -o errexit
 pip install --upgrade pip
-pip install -r ./requirements.txt
+pip3 freeze > requirements.txt
+pip install -r requirements.txt
 poetry install
 python manage.py collectstatic --no-input
 python manage.py migrate
