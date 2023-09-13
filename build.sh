aa7ea43e9169fe4868a4956a5c6779a5d0593a4d
dreadnought-admin <dom.alexan@gmail.com>
2023-09-13 17:31:46 -0600

    Tests requirements.txt

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 set -o errexit
 pip install --upgrade pip
-pip install django
-pip3 install django
-pip install django_rest_framework
-pip3 install django_rest_framework
-pip install django-cors-headers
-pip3 install django-cors-headers
-pip install whitenoise
-pip3 install whitenoise
-pip install psycopg2 gunicorn
-pip3 install psycopg2 gunicorn
+# pip install django
+# pip3 install django
+# pip install django_rest_framework
+# pip3 install django_rest_framework
+# pip install django-cors-headers
+# pip3 install django-cors-headers
+# pip install whitenoise
+# pip3 install whitenoise
+# pip install psycopg2 gunicorn
+# pip3 install psycopg2 gunicorn
 pip3 freeze > requirements.txt
 pip install -r requirements.txt
 pip3 install -r requirements.txt
