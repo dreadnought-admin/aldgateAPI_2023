7f0cf25c376f9e0f3ba9ab2a61dc4c4c91839024
dreadnought-admin <dom.alexan@gmail.com>
2023-09-13 16:57:04 -0600

    Adds Django to build

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 set -o errexit
 pip install --upgrade pip
+pip install django
+pip3 install django
 pip3 freeze > requirements.txt
 pip3 install -r requirements.txt
 python api/manage.py collectstatic --no-input
