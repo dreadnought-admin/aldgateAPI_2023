a021890951ab7c710703779a8bd1389440f2ba15
dreadnought-admin <dom.alexan@gmail.com>
2023-09-13 16:52:12 -0600

    Removes Poetry

--- build.sh
+++ build.sh
@@ -2,6 +2,5 @@ set -o errexit
 pip install --upgrade pip
 pip3 freeze > requirements.txt
 pip3 install -r requirements.txt
-poetry install
 python manage.py collectstatic --no-input
 python manage.py migrate
