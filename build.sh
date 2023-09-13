8596a706c5391421390bccd0194de76cb6e0cf6d
dreadnought-admin <dom.alexan@gmail.com>
2023-09-13 17:55:30 -0600

    Changes build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,23 @@
+# set -o errexit
+# pip install --upgrade pip
+# # pip install django
+# # pip3 install django
+# # pip install django_rest_framework
+# # pip3 install django_rest_framework
+# # pip install django-cors-headers
+# # pip3 install django-cors-headers
+# # pip install whitenoise
+# # pip3 install whitenoise
+# # pip install psycopg2 gunicorn
+# # pip3 install psycopg2 gunicorn
+# pip3 freeze > requirements.txt
+# pip install -r requirements.txt
+# pip3 install -r requirements.txt
+# python api/manage.py migrate
+
 set -o errexit
-pip install --upgrade pip
-# pip install django
-# pip3 install django
-# pip install django_rest_framework
-# pip3 install django_rest_framework
-# pip install django-cors-headers
-# pip3 install django-cors-headers
-# pip install whitenoise
-# pip3 install whitenoise
-# pip install psycopg2 gunicorn
-# pip3 install psycopg2 gunicorn
-pip3 freeze > requirements.txt
+
 pip install -r requirements.txt
-pip3 install -r requirements.txt
-python api/manage.py migrate
+
+python manage.py collectstatic --no-input
+python manage.py migrate
