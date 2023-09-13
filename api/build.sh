7b8e29c68a4f2399816eff017df978c8f8c8fa61
dreadnought-admin <dom.alexan@gmail.com>
2023-09-13 17:09:12 -0600

    Removes duplicate build.sh

--- api/build.sh
+++
@@ -1,6 +0,0 @@
-set -o errexit
-pip install --upgrade pip
-pip3 freeze > requirements.txt
-pip3 install -r requirements.txt
-python api/manage.py collectstatic --no-input
-python api/manage.py migrate
