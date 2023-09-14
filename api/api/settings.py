59f973f2f31045e8eab30390fa2352686b0367ff
dreadnought-admin <dom.alexan@gmail.com>
2023-09-14 00:13:50 -0600

    Creates new wsgi.py

--- api/api/settings.py
+++ api/api/settings.py
@@ -12,11 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+import django.core.handlers.wsgi
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+application = django.core.handlers.wsgi.WSGIHandler()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -132,6 +133,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
