076ce82539b8231d62cfea43d2572fcc2fea6c69
dreadnought-admin <dom.alexan@gmail.com>
2023-09-14 00:33:28 -0600

    Fixes wsgi.py

--- api/api/settings.py
+++ api/api/settings.py
@@ -88,7 +88,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'api.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
--- api/api/wsgi.py
+++ api/api/wsgi.py
@@ -8,10 +8,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
-sys.path.append('/home/development/ald-api/api')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
+
 application = get_wsgi_application()
